6959685e16759d16845aa8c12a68cd401fc41610
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2015-03-07 13:17:21 -0800

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject bidirectional "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.analyzer.jvm "0.3.0"]
+                 [org.clojure/jvm.tools.analyzer "0.6.1"
+                  :exclusions [org.clojure/clojure
+                               org.clojure/clojurescript]]])
