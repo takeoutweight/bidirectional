199988f4681d63eb20005e0d93d7c4c05852adf7
Nathan Sorenson <nathan.d.sorenson@gmail.com>
2015-03-28 20:55:25 -0700

    reminder of what the similar imports are for

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/tools.analyzer.jvm "0.3.0"]
-                 [org.clojure/jvm.tools.analyzer "0.6.1"
+                 [org.clojure/tools.analyzer.jvm "0.3.0"] ;; for most of the analysis
+                 [org.clojure/jvm.tools.analyzer "0.6.1" ;; just for emit-form
                   :exclusions [org.clojure/clojure
                                org.clojure/clojurescript]]])
